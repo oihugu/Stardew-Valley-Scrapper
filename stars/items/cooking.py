6b1c12602616e65f149806d984ff9eab5e73b603
oihugu <oihugu@gmail.com>
2023-04-03 02:25:29 -0300

    generalizing recipe source

---
+++ stars/items/cooking.py
@@ -0,0 +1,59 @@
+import regex
+
+# Extract the ingredients of a recipe in a infobox line, returning a list of lists with the name of the ingredient and the quantity
+def ectract_ingredients(line):
+    ingredients = {}
+    for a in (line.find('td', {'id': 'infoboxdetail'})):
+        if len(a) > 1:
+            _text = a.text.replace('\xa0', '').replace('\n', '').replace('\t', '').strip()
+            _text = regex.search(r'(.*)\(([0-9])\)', _text)
+            ingredients[_text[1]] = _text[2]
+    return ingredients
+
+
+# Extract Buffs from a line in the infobox, returning a dict with the buff name and the buff value
+def extract_buffs(line):
+    buffs = {}
+    _text = line.text.replace('Buff(s)','').replace('\xa0', '').replace('\n', '').replace('\t', '').strip()
+    _text = regex.search(r'(.*)\(([+-][0-9])\)', _text)
+    buffs[_text[1]] = _text[2]
+    return buffs
+
+def extract_recipe_source(line):
+    multiple_sources = []
+    _text = line.text.replace('Recipe Source(s)', '').replace('\xa0', '').replace('\t', '').strip()
+    
+    while '\n\n' in _text:
+        _text = regex.sub(r'\n\n', '\n', _text)
+    
+    for i, source in enumerate(_text.split('\n')):
+        recipe_source = {}
+        
+        if source != '':
+            
+            if regex.search(r'(.*)\(([A-z]+).*[+-].*([0-9][+-]).*\)', source): # From a friend
+                r_text = regex.search(r'(.*)\(([A-z]+).*[+-].*([0-9][+-]).*\)', source)
+                recipe_source['Font'] = 'Friendship'
+                recipe_source['Source'] = r_text.group(1)
+                recipe_source['Vehicle'] = r_text.group(2)
+                recipe_source['When'] = r_text.group(3)
+            
+            if "The Queen of Sauce" in source.strip():
+                source = _text.split('\n')[i+1].strip()
+                r_text = regex.search(r'([0-9]+) (.*), Year ([0-9]+)', source)
+                recipe_source['Font'] = 'The Queen of Sauce'
+                recipe_source['Day'] = r_text.group(1)
+                recipe_source['Season'] = r_text.group(2)
+                recipe_source['Year'] = r_text.group(3)
+
+            if 'Stardrop Saloon' in source.strip():
+                source = source.strip()
+                r_text = regex.search(r'Stardrop Saloon for .*"([0-9]+)".*', source)
+                recipe_source['Font'] = 'Stardrop Saloon'
+                recipe_source['Price'] = r_text.group(1) + 'g'
+
+            if recipe_source != {}:
+                multiple_sources.append(recipe_source)
+
+    return multiple_sources
+    
--- stars/items/items.py
+++ stars/items/items.py
@@ -1,10 +1,11 @@
+from .cooking import * 
 import regex
 
 # Extract the text that are before some image, placing the text in the key of an dictionary and the image name as the value
 # This is very useful for lines in the infobox that contains the image and the price for example, or itens that regenerates health or energy                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 def extract_text_before_image(line):
     elements = line.find_all('td')
-    ap = []
+    ap = {}
     for i, a in enumerate(elements):
         if a.find('img') != None:
             index_temp = i
@@ -14,16 +15,38 @@ def extract_text_before_image(line):
             text = ' '.join(text.split())  # remove espaços extras
 
             if len(text.split()) == 1:
-                ap.append([image_, text])
+                ap[image_] = text
     return ap
 
+# Utilizes dynamicaly other functions to extract the data from the infobox
+def extract_food_dynamic(data, line):
+    section = line.find('td', {'id': 'infoboxsection'}).text.replace('\n', '')
 
-# Extract the ingredients of a recipe in a infobox line, returning a list of lists with the name of the ingredient and the quantity
-def ectract_ingredients(line):
-    detail = []
-    for a in (line.find('td', {'id': 'infoboxdetail'})):
-        if len(a) > 1:
-            _text = a.text.replace('\xa0', '').replace('\n', '').replace('\t', '').strip()
-            _text = regex.search(r'(.*)\(([0-9])\)', _text)
-            detail.append([_text[1], _text[2]])
-    return detail
+    if section == 'Ingredients':
+        return ectract_ingredients(line)
+    
+    elif section == 'Buff(s)':
+        return extract_buffs(line)
+    
+    elif section in ['Energy / Health', 'Qi Seasoning']:
+        ext = extract_text_before_image(line)
+        if section == 'Qi Seasoning' and data['name'] in ext.keys():
+            ext['Price'] = ext[data['name']]
+            del ext[data['name']]
+        return ext 
+
+    elif section == 'Recipe Source(s)':
+        return extract_recipe_source(line)
+
+    else:
+        return line.find('td', {'id': 'infoboxdetail'}).text.replace('\xa0', '').replace('\n', '').replace('\t', '')
+
+def extract_dynamic(data, line):
+    if 'Source' not in data['lines'].keys():
+        return line.find('td', {'id': 'infoboxdetail'}).text.replace('\xa0', '').replace('\n', '').replace('\t', '')
+    
+    elif data['lines']['Source'] == 'Cooking':
+        return extract_food_dynamic(data, line)
+    
+    else:
+        return line.find('td', {'id': 'infoboxdetail'}).text.replace('\xa0', '').replace('\n', '').replace('\t', '')
